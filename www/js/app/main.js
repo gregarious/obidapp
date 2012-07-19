41d865c6abfb0ee80dcb86d2edaac04f70d72189
Greg Nicholas <greg.nicholas@gmail.com>
2012-07-19 15:29:03 -0400

    basic version of single pages working + feed links

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -21,46 +21,77 @@ $(function(){
 		specials: collectionBuilder('special')
 	};
 
+	Scenable.typeModelIdMap = {
+		places: function(id) { return '/api/v1/place/'+id+'/'; },
+		events: function(id) { return '/api/v1/event/'+id+'/'; },
+		specials: function(id) { return '/api/v1/special/'+id+'/'; }
+	};
+
 	$(document).bind( "pagebeforechange", function(e, data) {
+		var url, match, resourceType, collection, contentEl, statusEl, listEl, id;
 		if (typeof data.toPage === "string") {
-			var url = $.mobile.path.parseUrl(data.toPage);
-			var match = /^\#feed\?type\=(.+)/.exec(url.hash);
-			var section = (match[1]);
-			var collection = Scenable.typeCollectionMap[section];
+			url = $.mobile.path.parseUrl(data.toPage);
+			match = /^\#(.+)\?(.*)/.exec(url.hash);
+			if (match) {
+				var page = match[1];
+				var args = {};
+				if (match[2]) {
+					_.each(match[2].split("&"), function(clause){
+						var submatch = /(.*)=(.*)/.exec(clause);
+						if (submatch[1]) {
+							args[submatch[1]] = submatch[2];
+						}
+					});
+				}
 
-			var contentEl = $('#feed .content');
-			var statusEl = contentEl.find('.content-status');
-			var listEl = contentEl.find('.content-list');
+				if (page === 'feed') {
+					resourceType = args['type'];
+					collection = Scenable.typeCollectionMap[args['type']];
 
-			if (collection) {
-				listEl.hide();
-				statusEl.html('loading...').show();
+					contentEl = $('#feed .content');
+					statusEl = contentEl.find('.content-status');
+					listEl = contentEl.find('.content-list');
 
-				collection.fetch({
-					success: function(collection, response) {
-						listEl.html('<h3>'+section+'</h3>');
-						listEl.append('<ul>');
-						collection.each(function(model){
-							if(section!=='specials') {
-								listEl.append('<li>' + model.get('name') + '</li>');
-							}
-							else {
-								listEl.append('<li>' + model.get('title') + '</li>');
-							}
+					if (collection) {
+						listEl.hide();
+						statusEl.html('loading...').show();
+
+						collection.fetch({
+							success: function(collection, response) {
+								listEl.html('<h3>'+resourceType+'</h3>');
+								listEl.append('<ul data-role="listview">');
+								collection.each(function(model){
+									if(resourceType!=='specials') {
+										listEl.append('<li><a href="#single?type=' + resourceType + '&id=' + model.get('id') + '">' + model.get('name') + '</a></li>');
+									}
+									else {
+										listEl.append('<li><a href="#single?type=' + resourceType + '&id=' + model.get('id') + '">' + model.get('title') + '</a></li>');
+									}
+								});
+								listEl.append('</ul>').show();
+								statusEl.hide();
+							},
+							error: function(collection, response) {
+								console.log(response);
+								statusEl.html('error');
+							},
+							timeout: 2000
 						});
-						listEl.append('</ul>').show();
-						statusEl.hide();
-					},
-					error: function(collection, response) {
-						console.log(response);
-						statusEl.html('error');
-					},
-					timeout: 2000
-				});
-			}
-			else {
-				listEl.hide();
-				statusEl.html('internal error: invalid url').show();
+					}
+					else {
+						listEl.hide();
+						statusEl.html('internal error: invalid url').show();
+					}
+				}
+				else if (page === 'single') {
+					resourceType = args['type'];
+					collection = Scenable.typeCollectionMap[args['type']];
+
+					id = args['id'];
+					contentEl = $('#single .content');
+					var model = collection.get(Scenable.typeModelIdMap[resourceType](id));
+					contentEl.html(model.get('name'));
+				}
 			}
 		}
 	});
