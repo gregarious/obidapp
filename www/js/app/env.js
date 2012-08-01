bda73276f70c1dcbda5912774c5b16ccab601a7b
Greg Nicholas <greg.nicholas@gmail.com>
2012-07-31 22:21:56 -0400

    removed redundant Backbone sync setup

--- www/js/app/env.js
+++ www/js/app/env.js
@@ -4,16 +4,3 @@ Scenable = {
 		SITEURL: 'http://127.0.0.1:8000'
 	}
 };
-
-/*** Override Backbone's Model/Collection sync functions **/
-(function(){
-	var jsonpSync = function(method, model, options) {
-		var opts = options || {};
-		opts.dataType = "jsonp";
-		return Backbone.sync(method, model, opts);
-	};
-
-	Backbone.Model.prototype.sync = jsonpSync;
-	Backbone.Collection.prototype.sync = jsonpSync;
-})();
-
