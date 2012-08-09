c41429ab33398197d878dd9a955b2a6a4a0c6d0a
Greg Nicholas <greg.nicholas@gmail.com>
2012-08-09 16:33:45 -0400

    header text working for single pages

--- www/js/app/detail/views.js
+++ www/js/app/detail/views.js
@@ -16,7 +16,7 @@ define(["text!templates/single-place.html", "text!templates/single-event.html", 
 				var topbar = '<div class="topbar">'+
 				'<nav id="top-bar-single">'+
 				'<a class="button-back" href="#"><img src="http://localhost/obidapp/www/img/assets/back-btn.png"></a>'+
-				'<h1>{{name}}</h1></nav></div>';
+				'<h1>'+this.model.headerText()+'</h1></nav></div>';
 				this.$el.html(topbar);
 				if (this.template) {
 					this.$el.append(this.template(this.model.attributes));
