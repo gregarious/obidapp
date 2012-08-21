870f4508b9c9292bc5c1b757e90675f1876ca0b2
laras126 <laras126@gmail.com>
2012-08-21 18:14:32 -0400

    homescreen prompt text and zindex

--- www/js/main.js
+++ www/js/main.js
@@ -121,7 +121,7 @@ requirejs(['explore/controller', 'detail/controller'], function(exploreCtrl, det
 		var msg = '';
 		if (navigator.userAgent.match(/(iPhone|iPad)/)) {
 
-			$('.add-to-homescreen').delay(100).fadeIn(400);
+			$('.add-to-homescreen').delay(100).fadeIn(200);
 			$('.close').click(function(){
 				$('.add-to-homescreen').fadeOut(400);
 			});
